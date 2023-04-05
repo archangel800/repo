dbd560f346bd8ccdb1e080248c33a6ea8013fa84
Giorgi Dzidziguri <giorgidzidziguri27@gmail.com>
2023-04-05 11:57:05 +0400

    add in memory security

--- ch05/Implementing-inmemory-authentication/course-tracker-start/src/main/java/com/manning/sbip/ch05/security/SecurityConfiguration.java
+++ ch05/Implementing-inmemory-authentication/course-tracker-start/src/main/java/com/manning/sbip/ch05/security/SecurityConfiguration.java
@@ -1,4 +1,71 @@
 package com.manning.sbip.ch05.security;
 
-public class SecurityConfiguration {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    AccessDeniedHandler accessDeniedHandler;
+
+    protected void configure(HttpSecurity auth) throws Exception {
+        auth.authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/delete/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login")
+                .and()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+    }
+
+    @Override
+    public void configure(WebSecurity webSecurity) throws  Exception {
+        webSecurity.ignoring().antMatchers("/webjars/**", "/images/**", "/css/**", "/h2-console/**");
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder.inMemoryAuthentication().passwordEncoder(passwordEncoder())
+                .withUser("user")
+                .password(passwordEncoder().encode("giorguna"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(passwordEncoder().encode("giorguna"))
+                .roles("ADMIN");
+    }
+
+//    @Bean
+//    @Override
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails user = User.withUsername("user")
+//                .passwordEncoder(passwordEncoder()::encode)
+//                .password("user")
+//                .roles("USER")
+//                .build();
+//        UserDetails admin = User.withUsername("admin")
+//                .passwordEncoder(passwordEncoder()::encode)
+//                .password("admin")
+//                .roles("ADMIN")
+//                .build();
+//
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(user);
+//        manager.createUser(admin);
+//
+//        return manager;
+//    }
+
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+
 }
